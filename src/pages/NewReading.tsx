f42cdadb2b78a5e7f6ad8e016e262c3b57ab8915
jlopes <joao.victor@lojainterativa.com>
2025-07-07 14:36:45 -0300

    Cria o componente NewReading para exibir a seção de nova tiragem no aplicativo

---
+++ src/pages/NewReading.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NewReading: React.FC = () => {
+  return (
+    <div>
+      <h2>Nova Tiragem</h2>
+      <p>Aqui você poderá jogar cartas e ver uma interpretação.</p>
+    </div>
+  );
+};
+
+export default NewReading;
